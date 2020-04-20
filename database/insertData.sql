e962f1fb8e96fd6ccd392568ac889463cbd6a2f3
JordanRamsey <jrams1414@gmail.com>
2020-04-19 22:45:53 -0400

    member views

--- database/insertData.sql
+++ database/insertData.sql
@@ -6,11 +6,24 @@ delete from member;
 delete from book;
 
 insert into member(name, phone, email, balance) values ('Jordan', '738-222-4533', 'email@gmail.com', '0');
+insert into member(name, phone, email, balance) values ('Bob', '738-322-4533', 'email@gmail.com', '0');
+insert into member(name, phone, email, balance) values ('James', '222-322-4893', 'email@gmail.com', '0');
+
+
 
 insert into bookItem(isbn) values('978-3-16-148410-0');
+insert into bookItem(isbn) values('549-3-16-148410-0');
+insert into bookItem(isbn) values('222-3-16-148410-0');
+insert into bookItem(isbn) values('456-3-16-148410-0');
 
-insert into loan(b_id, m_id, checkout_date, due_date, l_id)  values ('1', '1', '02-20-2020', '03-20-2020', '1');
+
+insert into loan(b_id, m_id, checkout_date, due_date, l_id, status)  values ('1', '1', '02-20-2020', '03-20-2020', '1', 'active');
 
 insert into book(ISBN, author, title, year, pages)  values ('978-3-16-148410-0', 'Miller','Pandemic','2020','330');
+insert into book(ISBN, author, title, year, pages)  values ('549-3-16-148410-0', 'Johnson','Search','1990','500');
+insert into book(ISBN, author, title, year, pages)  values ('222-3-16-148410-0', 'James','Legend','1990','500');
+insert into book(ISBN, author, title, year, pages)  values ('456-3-16-148410-0', 'Thomas','The Hill','1990','500');
+
+
 
 insert into librarian(username, phone, email, password) values ('admin', '483-433-4334', 'admin@gmail.com', 'password');
---
+++ views/member.py
@@ -0,0 +1,65 @@
+from app import app, get_db
+from flask import Flask, request, jsonify, make_response
+
+@app.route('/status/<m_id>', methods = ['GET'])
+def getStatus(m_id):
+    db = get_db()
+    cur = db.cursor()
+
+    try:
+        # Retrieve member info
+        cur.execute('SELECT * FROM member where ID = ?', (m_id))
+        member = cur.fetchone()
+
+        member_data = {}
+        member_data['name'] = member['name']
+        member_data['balance'] = member['balance']
+
+        # Retrieve members current loans
+        cur.execute("SELECT * FROM loan INNER JOIN bookItem on loan.b_id = bookItem.ID INNER JOIN book on bookItem.isbn = book.ISBN WHERE loan.m_id = ? AND loan.status = 'active'", (m_id))
+        loans = cur.fetchall()
+
+        # Retrieve the members fines
+        cur.execute("SELECT * FROM fine INNER JOIN loan on fine.l_id = loan.ID WHERE loan.m_id = ?", (m_id))
+        fines = cur.fetchall()
+
+        loans_output = []
+        for loan in loans:
+            data = {}
+            data['b_id'] = loan['b_id']
+            data['title'] = loan['title']
+            data['author'] = loan['author']
+            data['return_date'] = loan['due_date']
+            loans_output.append(data)
+
+        fines_output = []
+        for fine in fines:
+            data = {}
+            data['amount'] = fine['amount']
+            fines_output.append(data)
+        
+        response_object = {
+            'status': 'success',
+            'member': member_data,
+            'loans': loans_output,
+            'fines': fines_output
+        }
+
+        return jsonify(response_object)
+    
+    except Exception as ex:
+        print(ex)
+        response_object = {
+            'status': 'fail'
+        }
+        return jsonify(response_object)
+
+
+
+
+
+
+    for row in cur:
+        print(row['name'])
+
+    return 'success'
